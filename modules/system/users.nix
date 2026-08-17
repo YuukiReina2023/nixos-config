e0f3e376b7a2e6013d3e49041cd616be58dcfd85
Yuuki Reina 結城翎奈 <lycorismisaka@gmail.com>
2026-08-17 18:35:25 +0800

    Update user and group names to lowercase
    Changed username from 'YuukiReina2023' to lowercase 'yuukireina2023' and created corresponding group.

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 {
-
-
   programs.fish.enable = true;
 
-  users.users.YuukiReina2023 = {
+  # 将 YuukiReina2023 修改为全小写的 yuukireina2023
+  users.users.yuukireina2023 = {
     isNormalUser = true;
     description = "YuukiReina2023";
+    group = "yuukireina2023"; # 明确指定主组名称
     extraGroups = [
       "networkmanager"
       "wheel"
@@ -18,4 +18,7 @@
     # shell = pkgs.bash;
     shell = pkgs.fish;
   };
+
+  # 建立对应的同名主组
+  users.groups.yuukireina2023 = {};
 }
