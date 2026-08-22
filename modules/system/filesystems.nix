04537e5e7453944bf883fbcdc60ad0c45824148a
Yuuki Reina 結城翎奈 <yuukireina2023@gmail.com>
2026-08-22 16:36:25 +0800

    Configure auto-mount for Windows data and EFI partitions
    Updated filesystem configuration to mount Windows partitions with appropriate options.

--- modules/system/filesystems.nix
+++ modules/system/filesystems.nix
@@ -1,11 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  # Auto mount
+  # 挂载 Windows 数据分区 (nvme0n1p2)
   fileSystems."/run/media/yuukireina2023/lw" = {
-    device = "/dev/disk/by-label/lw";
-    fsType = "ext4";
+    device = "/dev/nvme0n1p2";
+    fsType = "ntfs3"; # 使用 kernel 内置的高性能 ntfs3 驱动
     options = [
-      "defaults"
+      "rw"
+      "nofail"
+      "uid=1000"         # 1000 通常是你的主用户 (yuukireina2023) 的 UID
+      "gid=100"          # 100 是 users 组的 GID
+      "fmask=0022"       # 设置文件权限 (允许读写)
+      "dmask=0022"       # 设置文件夹权限
+      "windows_names"    # 防止创建 Windows 不支持的特殊字符文件名
+    ];
+  };
+
+  # 挂载 Windows EFI 分区 (用于 systemd-boot 跨盘识别 Windows 引导)
+  fileSystems."/boot-win" = {
+    device = "/dev/disk/by-partuuid/9783550e-5107-4e02-ac1a-a3f17d727617";
+    fsType = "vfat";
+    options = [
+      "ro"
       "nofail"
     ];
   };
