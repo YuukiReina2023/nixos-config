b2b432f8a67d81c2ca0e2e3d7cd98228e2c1a2e6
YuukiReina2023 <YuukiReina2023@users.noreply.github.com>
2026-09-07 16:02:08 +0800

    feat(gitconfig): 添加 GitHub 镜像加速配置

--- modules/home/deploy-files.nix
+++ modules/home/deploy-files.nix
@@ -4,4 +4,17 @@
     source = ../scripts/niri-screenshot.sh;
     executable = true;
   };
+
+  # GitHub 镜像加速：使用 fastgit / raw.fastgit 以及 ghproxy 作为备用
+  home.file.".gitconfig".text = ''
+    [url "https://hub.fastgit.org/"]
+      insteadOf = https://github.com/
+    [url "git@hub.fastgit.org:"]
+      insteadOf = git@github.com:
+    [url "https://raw.fastgit.org/"]
+      insteadOf = https://raw.githubusercontent.com/
+    # 备用：ghproxy（如需启用可取消注释）
+    # [url "https://ghproxy.com/https://github.com/"]
+    #   insteadOf = https://github.com/
+  '';
 }
