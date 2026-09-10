2a6b1468b4041de94840d5e4f1786d3d958fc3f6
YuukiReina2023 <yuukireina2023@gmail.com>
2026-09-10 15:51:48 +0800

    refactor: 更新 Git 配置，禁用失效的 FastGit 镜像，使用官方 GitHub 地址

--- modules/home/deploy-files.nix
+++ modules/home/deploy-files.nix
@@ -5,16 +5,9 @@
     executable = true;
   };
 
-  # GitHub 镜像加速：使用 fastgit / raw.fastgit 以及 ghproxy 作为备用
+  # 使用官方 GitHub 地址，禁用失效的 FastGit / raw.fastgit 重写
   home.file.".gitconfig".text = ''
-    [url "https://hub.fastgit.org/"]
-      insteadOf = https://github.com/
-    [url "git@hub.fastgit.org:"]
-      insteadOf = git@github.com:
-    [url "https://raw.fastgit.org/"]
-      insteadOf = https://raw.githubusercontent.com/
-    # 备用：ghproxy（如需启用可取消注释）
-    # [url "https://ghproxy.com/https://github.com/"]
-    #   insteadOf = https://github.com/
+    # GitHub should use the official upstream URLs.
+    # Third-party mirrors are intentionally disabled because they may fail or block access.
   '';
 }
