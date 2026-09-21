dfa26468710a35c00f8a84afabb01d6ee7d2a2db
YuukiReina2023 <yuukireina2023@gmail.com>
2026-09-21 19:35:21 +0800

    fix(nix): place nix.extraOptions inside module attribute set

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -102,11 +102,11 @@
     system-config-printer
     epsonscan2
   ];
-}
+  # Ensure Cachix substituters and trusted keys are available to the Nix daemon
+  # (use nix.extraOptions to write to /etc/nix/nix.conf so the daemon trusts the caches)
+  nix.extraOptions = ''
+  substituters = https://cache.nixos.org https://nix-community.cachix.org https://noctalia.cachix.org
+  trusted-public-keys = nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=
+  '';
 
-# Ensure Cachix substituters and trusted keys are available to the Nix daemon
-# (use nix.extraOptions to write to /etc/nix/nix.conf so the daemon trusts the caches)
-nix.extraOptions = ''
-substituters = https://cache.nixos.org https://nix-community.cachix.org https://noctalia.cachix.org
-trusted-public-keys = nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=
-'';
+}
